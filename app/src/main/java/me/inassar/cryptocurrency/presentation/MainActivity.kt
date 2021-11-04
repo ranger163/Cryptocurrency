9f946e10c61f4f6b18198ccc4ef4566e3700b12e
ranger163 <ahmed.nassar163@gmail.com>
2021-11-04 13:38:16 +0300

    Finishing Up Modified: - Adding navHost to MainActivity.kt to handle screen routing.

--- app/src/main/java/me/inassar/cryptocurrency/presentation/MainActivity.kt
+++ app/src/main/java/me/inassar/cryptocurrency/presentation/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import me.inassar.cryptocurrency.presentation.coinDetails.CoinDetailScreen
+import me.inassar.cryptocurrency.presentation.coinList.CoinListScreen
 import me.inassar.cryptocurrency.presentation.ui.theme.CoinPaprikaPracticeTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +21,24 @@ class MainActivity : ComponentActivity() {
             CoinPaprikaPracticeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = ScreenRout.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = ScreenRout.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = ScreenRout.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CoinPaprikaPracticeTheme {
-        Greeting("Android")
     }
 }
